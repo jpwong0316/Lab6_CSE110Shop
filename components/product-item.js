9c3446f6055101a3a06a0de29c919fd0449bbd83
Jason Wong <jpwong@ucsd.edu>
2021-02-13 20:54:04 -0800

    finished lab

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,99 @@
 // product-item.js
 
 class ProductItem extends HTMLElement {
-  // TODO
+  constructor() {
+    super();
+    let shadow = this.attachShadow({mode: 'open'});
+    let style = document.createElement('style');
+    style.textContent = `
+    .price {
+      color: green;
+      font-size: 1.8em;
+      font-weight: bold;
+      margin: 0;
+    }
+    
+    .product {
+      align-items: center;
+      background-color: white;
+      border-radius: 5px;
+      display: grid;
+      grid-template-areas: 
+      'image'
+      'title'
+      'price'
+      'add';
+      grid-template-rows: 67% 11% 11% 11%;
+      height: 450px;
+      filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+      margin: 0 30px 30px 0;
+      padding: 10px 20px;
+      width: 200px;
+    }
+    
+    .product > button {
+      background-color: rgb(255, 208, 0);
+      border: none;
+      border-radius: 5px;
+      color: black;
+      justify-self: center;
+      max-height: 35px;
+      padding: 8px 20px;
+      transition: 0.1s ease all;
+    }
+    
+    .product > button:hover {
+      background-color: rgb(255, 166, 0);
+      cursor: pointer;
+      transition: 0.1s ease all;
+    }
+    
+    .product > img {
+      align-self: center;
+      justify-self: center;
+      width: 100%;
+    }
+    
+    .title {
+      font-size: 1.1em;
+      margin: 0;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+    
+    .title:hover {
+      font-size: 1.1em;
+      margin: 0;
+      white-space: wrap;
+      overflow: auto;
+      text-overflow: unset;
+    }`;
+
+    let li = document.createElement('li');
+    li.setAttribute('class', 'product');
+
+    let img = document.createElement('img');
+    img.width = "200";
+    li.appendChild(img);
+
+    let p1 = document.createElement('p');
+    p1.setAttribute('class', 'title');
+    li.appendChild(p1);
+
+    let p2 = document.createElement('p');
+    p2.setAttribute('class', 'price');
+    li.appendChild(p2);
+
+    let button = document.createElement('button');
+    button.setAttribute('onclick', "alert('Add to Cart!')");
+    button.textContent = "Add to Cart";
+    
+    li.appendChild(button);
+
+    shadow.appendChild(style);
+    shadow.appendChild(li);
+  }
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,61 @@
 // Script.js
 
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  //localStorage.clear();
+  if (localStorage.getItem('item1') == null) {
+    fetchArray();
+  } else {
+    setupPage();
+  } 
 });
+
+function setupPage() {
+  for (let i = 1; i < 21; i++) {
+    let tempData = JSON.parse(localStorage.getItem('item' + i));
+    let temp = document.createElement('product-item');
+    temp.shadowRoot.querySelector("img").src = tempData.image;
+    temp.shadowRoot.querySelector("img").alt = tempData.title;
+    temp.shadowRoot.querySelector(".title").textContent = tempData.title;
+    temp.shadowRoot.querySelector(".price").textContent = '$' + tempData.price;
+    let button = temp.shadowRoot.querySelector("button");
+    button.addEventListener("click", buttonPress);
+    document.getElementById("product-list").appendChild(temp);
+
+    if (localStorage.getItem(tempData.title) != null) {
+      button.setAttribute('onclick', "alert('Remove from Cart!')");
+      button.textContent = "Remove from Cart";
+      document.getElementById("cart-count").textContent =
+        (Number(document.getElementById("cart-count").textContent) + 1);
+    }
+  }
+}
+
+function buttonPress() {
+  let count = document.getElementById("cart-count");
+  let title = this.parentElement.querySelector(".title").textContent;
+  if (this.textContent == "Add to Cart") {
+    this.setAttribute('onclick', "alert('Remove from Cart!')");
+    this.textContent = "Remove from Cart";
+    count.textContent = (Number(count.textContent) + 1);
+    localStorage.setItem(title, "{title: " + title + " }");
+  } else {
+    this.setAttribute('onclick', "alert('Add to Cart!')");
+    this.textContent = "Add to Cart";
+    count.textContent = (Number(count.textContent) - 1);
+    localStorage.removeItem(title);
+  }
+}
+
+async function fetchArray() {
+  let response = await fetch('https://fakestoreapi.com/products');
+  if (response.ok) {
+    let data = await response.json();
+    for (let i = 0; i < 20; i++) {
+      localStorage.setItem('item' + (i+1), JSON.stringify(data[i]));
+    }
+    setupPage();
+  } else {
+    alert("Error: " + response.status);
+  }
+  return;
+}
